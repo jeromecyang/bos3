6ec72d0fdc663b8279e727dbe0cfb9657f15445a
Jerome Yang <cryang1018@gmail.com>
2019-04-06 21:24:58 -0400

    Add item

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 "use es6";
 
+class Item extends React.Component {
+  render() {
+    const { titlezh, urlzh } = this.props.item;
+    return (
+      <div>
+        <a href={urlzh} target="_blank">
+          {titlezh}
+        </a>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { items: [] };
   }
 
   componentDidMount() {
@@ -10,18 +24,36 @@ class App extends React.Component {
       .get(
         "https://spreadsheets.google.com/feeds/list/1PPCcW1XVljZ1n4plGoyYj44ciCMwklnBkeHayg0uQFE/1/public/values?alt=json"
       )
-      .then(function(response) {
-        const {
-          data: {
-            feed: { entry }
-          }
-        } = response;
-        console.log(entry);
-      });
+      .then(
+        function(response) {
+          const {
+            data: {
+              feed: { entry }
+            }
+          } = response;
+          const items = entry.map(e =>
+            Object.keys(e).reduce(
+              (collect, key) =>
+                key.includes("gsx$")
+                  ? { ...collect, [key.replace("gsx$", "")]: e[key].$t }
+                  : collect,
+              {}
+            )
+          );
+          this.setState({ items });
+        }.bind(this)
+      );
   }
 
   render() {
-    return <div>Lifetime Sojourner</div>;
+    return (
+      <div>
+        <h1>Lifetime Sojourner</h1>
+        {this.state.items.map(item => (
+          <Item item={item} />
+        ))}
+      </div>
+    );
   }
 }
 
